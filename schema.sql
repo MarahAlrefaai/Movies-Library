740d58e122b1b4f6c104a187c229f2a4cdcb651e
MarahAlrefaai <marahalrefaai17@gmail.com>
2022-02-01 22:56:27 +0200

    this is task 12 v2

---
+++ schema.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS favmovie;
+
+CREATE TABLE IF NOT EXISTS favmovie(
+   release VARCHAR(10000),
+    poster_path VARCHAR(10000),
+     overview VARCHAR(10000),
+     comment VARCHAR(225)
+    
+)
