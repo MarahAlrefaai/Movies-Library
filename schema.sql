e866f2d6fe7271d6d23a49f06bb174f14130d1d0
MarahAlrefaai <marahalrefaai17@gmail.com>
2022-02-02 15:00:25 +0200

    this is task 14 v1

--- schema.sql
+++ schema.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS favMovies;
+DROP TABLE IF EXISTS favmovies;
 
-CREATE TABLE IF NOT EXISTS favMovies(
+CREATE TABLE IF NOT EXISTS favmovies(
     id SERIAL PRIMARY KEY,
     title VARCHAR(255),
     release_date VARCHAR(10000),
